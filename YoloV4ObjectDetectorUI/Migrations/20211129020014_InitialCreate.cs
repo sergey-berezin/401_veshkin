647b401eb1100af55c5492b506b869e663c7e02e
Artem <46985381+ArtemVeshkin@users.noreply.github.com>
2021-11-29 06:12:44 +0300

    Persistent Storage code for Yolo Object Detector (Task 3)

---
+++ YoloV4ObjectDetectorUI/Migrations/20211129020014_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YoloV4ObjectDetectorUI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DetectedObjectDetails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Image = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetectedObjectDetails", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetectedObjects",
+                columns: table => new
+                {
+                    ObjectId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClassName = table.Column<string>(nullable: true),
+                    X = table.Column<int>(nullable: false),
+                    Y = table.Column<int>(nullable: false),
+                    Width = table.Column<int>(nullable: false),
+                    Height = table.Column<int>(nullable: false),
+                    DetailsID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetectedObjects", x => x.ObjectId);
+                    table.ForeignKey(
+                        name: "FK_DetectedObjects_DetectedObjectDetails_DetailsID",
+                        column: x => x.DetailsID,
+                        principalTable: "DetectedObjectDetails",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetectedObjects_DetailsID",
+                table: "DetectedObjects",
+                column: "DetailsID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetectedObjects");
+
+            migrationBuilder.DropTable(
+                name: "DetectedObjectDetails");
+        }
+    }
+}
